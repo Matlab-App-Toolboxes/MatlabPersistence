21fa02f0db2155f1bf4bcdb414c91b24d381d2c0
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2016-04-28 18:31:57 +0300

    updated pom as per matlab-simple-archetype

--- pom.m
+++ pom.m
@@ -1,19 +1,38 @@
 project_name = 'matlab-hdf5-persistence';
 
+% dependecies from git (or) math exchange
+
+dependecies(1).name = 'mmockito';
+dependecies(1).url = 'https://github.com/ragavsathish/mmockito.git';
+dependecies(1).scope = 'test';
+
+dependecies(2).name = 'jsonlab';
+dependecies(2).url = 'https://github.com/fangq/jsonlab.git';
+dependecies(2).scope = 'dev';
+
+
 addpath(genpath(pwd));
 cp = getClassPath();
+cd('lib')
 
-% dependecies from git (or) math exchange
-cd(cp.lib)
+pomPath = @(p) fullfile(cp.lib, p, 'pom.m');
+deletePom = @(p) delete(pomPath(p));
 
-% Test dependency
-if ~ exist('mmockito', 'dir')
-    git clone 'https://github.com/ragavsathish/mmockito.git'
+% clone libraries if not exist and delete 'pom.m'
+
+for i = 1:numel(dependecies)
+    d = dependecies(i);
+    
+    if ~ exist(d.name, 'dir')
+        git('clone', d.url);
+        
+        if exist(pomPath(d.name), 'file')
+            deletePom(d.name);
+        end
+    end
 end
 
-if ~ exist('jsonlab', 'dir') && strcmp(scope, 'test')
-    git clone 'https://github.com/fangq/jsonlab.git'
-end
-
+% add new libraries to path
+addpath(genpath(pwd));
 cd(cp.root)
 
