e57023d50a0a06897b146d7f62e03cec86277a5d
Sathish Narayanan <ragav.sathish@gmail.com>
2017-11-03 15:48:15 +0200

    Add verstaile javapath using toolbox

---
+++ src/main/matlab/+mpa/javaaddpath.m
@@ -0,0 +1,13 @@
+function javaaddpath()
+
+% Add the following jars to matlab dyanamic path for easy execution
+% 1) mpa-jutil-0.0.1-SNAPSHOT.jar   - provides marshalling and filtering support for persistence xml and orm xml
+% 2) java-uuid-generator-3.1.4.jar  - provides way to generate unique id based on date-time
+
+packageRoot = fileparts(mfilename('fullpath'));
+rootPath = fileparts(fileparts(fileparts(fileparts(packageRoot))));
+javaaddpath(fullfile(rootPath, 'lib', 'mpa-jutil-0.0.1-SNAPSHOT.jar'));
+javaaddpath(fullfile(rootPath, 'lib', 'java-uuid-generator-3.1.4.jar'));
+
+end
+
